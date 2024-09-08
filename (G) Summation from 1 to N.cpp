0a7a62e97e66d6fbb1e4d7b20eedccca7c46a6aa
Eng-omar999 <85528265+Eng-omar999@users.noreply.github.com>
2024-09-08 04:13:43 +0300

    (G) Summation from 1 to N.cpp

--- (G) Summation from 1 to N.cpp
+++ (G) Summation from 1 to N.cpp
@@ -5,13 +5,7 @@
 using namespace std;
 int main ()
 {   
- double x,y;
- cin >> x >>y;
- double originalNumber = x / y ; 
- 
- 
-cout << "floor " << x << " / " << y << " = " << floor(originalNumber)<<endl;
-cout << "ceil " << x << " / " << y << " = " << ceil(originalNumber)<<endl;
-cout << "round " << x << " / " << y << " = " << round(originalNumber)<<endl;
- 
+   long long n ;
+cin >> n ;
+cout << n * ( n + 1 ) / 2 ;
 }
