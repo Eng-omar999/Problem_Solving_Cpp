a6331e960cc0af8f59283d066d8f3de3c8210ca7
Eng-omar999 <85528265+Eng-omar999@users.noreply.github.com>
2024-09-08 04:28:21 +0300

    (N) Char.cpp

--- (N) Char.cpp
+++ (N) Char.cpp
@@ -1,48 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+int main ()
+{  
+    char x;
+    cin>>x;
  
-int main(){
-string equ;
-cin>>equ;
-int indexOpr;
-char charOpr;
-string num1,num2;
-int num1Int,num2Int;
-    
-    for(int i=0; i<=equ.size()-1; i++)
-    {
-        if(equ[i] == '+' || equ[i] == '-' || equ[i] == '*' || equ[i] == '/')
-        {
-            indexOpr= i;
-            charOpr= equ[i];
-            break;
-        }
-    }    
-    num1 =  equ.substr(0,indexOpr);
-    num2 =  equ.substr(indexOpr+1);
+  if( x <= 90 && 65 <= x) // CAP_alphabets
+   {
+    char After = x + 32;
+    cout<< After <<endl;
  
-    stringstream s1;
-    s1<<num1;
-    s1>>num1Int;
+   }
  
-    stringstream s2;    
-    s2<<num2;
-    s2>>num2Int;
+   else if(x <= 122 && 97<= x) // small_alphabets
+   {
+    char After = x - 32;
+    cout<< After <<endl;
  
-    if(charOpr == '+')
-    {
-        cout<< num1Int+num2Int;
-    }   else if(charOpr == '-')
-    {
-        cout<< num1Int-num2Int;
-    }       else if(charOpr == '*')
-    {
-        cout<< num1Int*num2Int;
-    }           else if(charOpr == '/')
-    {
-        cout<< num1Int/num2Int;
-    }               else{
-     return 0;
-}
  
-}
+   }
+}    
