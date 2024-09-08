bd73167401373892357875e78eccab1cc1e14240
Eng-omar999 <85528265+Eng-omar999@users.noreply.github.com>
2024-09-08 04:34:00 +0300

    (Y) The last 2 digits.cpp

--- (Y) The last 2 digits.cpp
+++ (Y) The last 2 digits.cpp
@@ -3,15 +3,16 @@ using namespace std;
  
 int main(){
  
-   double num1,num2,num3,num4;
+   int num1,num2,num3,num4;
    cin>>num1>>num2>>num3>>num4;
    
-   double powNum1,powNum2;
-   powNum1 = num2*log(num1);
-   powNum2 = num4*log(num3);
-   if(powNum1>powNum2){
-    cout<<"YES";
-   }else{
-    cout<<"NO";
-   }
+   int multi = (num1%100)*(num2%100)*(num3%100)*(num4%100);
+ 
+    multi = multi%100;
+    
+    if(multi<=9){
+    cout<<"0"<<multi;
+    }else{
+    cout<<multi;
+    }
 }
