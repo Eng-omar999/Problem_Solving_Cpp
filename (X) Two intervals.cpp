136dfe46a2b91fbccc86ee3ca5d78add8fb6ef73
Eng-omar999 <85528265+Eng-omar999@users.noreply.github.com>
2024-09-08 04:33:33 +0300

    (X) Two intervals.cpp

--- (X) Two intervals.cpp
+++ (X) Two intervals.cpp
@@ -5,14 +5,23 @@ int main(){
  
    int num1,num2,num3,num4;
    cin>>num1>>num2>>num3>>num4;
+int start,end;
+if((num3 < num1 && num4 <num1) || (num3 > num2 && num4 > num2)){
+cout<<-1<<endl;
+} else {
+   if(num1>num3){
+   start=num1;
+   }else{
+   start=num3;
+  
    
-   int multi = (num1%100)*(num2%100)*(num3%100)*(num4%100);
- 
-    multi = multi%100;
+   }if(num4<num2){
+    end=num4;
     
-    if(multi<=9){
-    cout<<"0"<<multi;
-    }else{
-    cout<<multi;
-    }
+   }else {
+        end=num2;
+   }
+   cout<<start<<" "<<end;
+   
+}
 }
