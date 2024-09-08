f39d50fb1c4c55002f2540e0b6f2d2e50a23c55c
Eng-omar999 <85528265+Eng-omar999@users.noreply.github.com>
2024-09-08 04:32:45 +0300

    (V) Comparison.cpp

--- (V) Comparison.cpp
+++ (V) Comparison.cpp
@@ -2,26 +2,17 @@
 using namespace std;
 int main(){
  
-   int num1,num2,res;
-   char s,eqal;
-   cin>>num1>>s>>num2>>eqal>>res;
-   int sum = num1+num2;
-   int min = num1-num2;
-   int mlt = num1*num2;
- 
-   if(sum == res && s == '+')//=+
+   int num1,num2;
+   char s;
+   cin>>num1>>s>>num2;
+   if(num1>num2 && s == '>')
    {
-    cout<<"Yes"<<endl;
-   }else if(sum != res && s == '+'){//!+
-        cout<<sum<<endl;
-   }else if(min == res && s == '-'){//=-
-        cout<<"Yes"<<endl;
- 
-    }else if(min != res && s == '-'){//!-
-        cout<<min<<endl;
-   }else if(mlt == res && s == '*'){
-        cout<<"Yes"<<endl;
-    }else if(mlt != res && s == '*'){
-        cout<<mlt<<endl;
-}
+    cout<<"Right"<<endl;
+   }else if(num1<num2 && s == '<'){
+        cout<<"Right"<<endl;
+   }else if(num1==num2 && s == '='){
+        cout<<"Right"<<endl;
+   }else{
+        cout<<"Wrong"<<endl;
+   }
 }
